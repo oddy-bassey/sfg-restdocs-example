93f1f55568afb8fe1847887d0c3d6a14b0c102ad
@revoltcode <oddybassey@gmail.com>
2021-02-21 15:42:56 +0100

    documenting generation. closes #8

--- target/generated-sources/annotations/com/revoltcode/sfgrestdocsexample/web/mappers/BeerMapperImpl.java
+++ target/generated-sources/annotations/com/revoltcode/sfgrestdocsexample/web/mappers/BeerMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-02-21T12:58:18+0100",
+    date = "2021-02-21T15:32:09+0100",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.7-ea (Debian)"
 )
 @Component
